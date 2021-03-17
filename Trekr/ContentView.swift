68a77f451bf5eb6092204a83b3c55bdbfba68389
Dan Akmat <akmatdan@gmail.com>
2021-03-17 16:22:20 +0600

    test2

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ScrollView {
-            Image("highlandss")
+            Image("highlands")
                 .resizable()
                 .scaledToFit()
             
