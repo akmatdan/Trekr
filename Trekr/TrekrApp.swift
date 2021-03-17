78f91840052c092d7420004e05e863d0449bafa3
Dan Akmat <akmatdan@gmail.com>
2021-03-17 16:11:55 +0600

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Daniil Akmatov on 16/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
