0c085e9f0f9b9079836b3e1616f444b52485f7c2
Dan Akmat <akmatdan@gmail.com>
2021-03-18 13:23:04 +0600

    I've just parsed the JSON file but it's only for one data of location.

---
+++ Trekr/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  Trekr
+//
+//  Created by Daniil Akmatov on 18/3/21.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+    
+    static let expamle = Location(id: 1, name: "Greate Smokey Mountains", country: "United State", description: "A greate place to visit", more: "More text here.", latitude: 35.6532, longitude: -83.5070, heroPicture: "smokies", advisory: "Bewate of the beare")
+}
